ee58e5615225b3052140bda6f95ab2da750d3607
Michelle Slee <sleemichelle@gmail.com>
2020-11-01 11:44:48 +0000

    First commit

---
+++ Challenge/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float timeDelay = 1.0f;
+    private float timestamp;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Press spacebar, send dog, there is a time delay
+        if (Time.time >= timestamp && (Input.GetKeyDown(KeyCode.Space)))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timestamp = Time.time + timeDelay;
+        }
+    }
+}
